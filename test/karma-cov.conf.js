801bcda4a48588cacf907e994f6782253ef70df4
Steven Silvester <steven.silvester@ieee.org>
2015-12-31 05:32:36 -0600

    Switch to webpack

--- test/karma-cov.conf.js
+++ test/karma-cov.conf.js
@@ -1,9 +1,15 @@
+<<<<<<< HEAD
+=======
+var path = require('path');
+
+>>>>>>> a21d194... Switch to webpack
 module.exports = function (config) {
   config.set({
     basePath: '..',
     browsers: ['Firefox'],
     frameworks: ['mocha'],
     reporters: ['mocha', 'coverage'],
+<<<<<<< HEAD
     files: [
       { pattern: 'lib/*.*', included: false },
       { pattern: 'package.json', included: false },
@@ -12,6 +18,29 @@ module.exports = function (config) {
       'node_modules/steal/steal.js',
       'test/karma.bootstrap.js'
     ],
+=======
+    preprocessors: { 'test/src/*.ts': ['webpack'] },
+    files: ['test/src/*.ts'],
+    webpack: {
+      resolve: {
+        extensions: ['', '.ts', '.js']
+      },
+      module: {
+        loaders: [
+          { test: /\.ts$/, loader: 'ts-loader' },
+          { test: /\.css$/, loader: 'style-loader!css-loader' },
+        ],
+        preLoaders: [
+          // instrument only testing sources with Istanbul
+          {
+            test: /\.js$/,
+            include: path.resolve('lib/'),
+            loader: 'istanbul-instrumenter'
+          }
+        ]
+      }
+    },
+>>>>>>> a21d194... Switch to webpack
     coverageReporter: {
       reporters : [
         { 'type': 'text' },
